50996e5cd6381c481b5045aa3ca0df94b6ec00c2
Matthias Beaupère <matthias.beaupere@gmail.com>
2016-02-08 17:50:58 +0100

    ajout du service de requete dans un fichier à part

--- www/js/app.js
+++ www/js/app.js
@@ -56,22 +56,3 @@ myApp.config(function($stateProvider, $urlRouterProvider) {
 
 	$urlRouterProvider.otherwise('/home');
 });
-
-myApp.factory('request', function() {
-	// Using a factory to fetch requests from WordPress site using WP REST API
-
-	var header = {
-		// The string "user:password" might have to be encoded in base64
-		'Authorization' : 'Basic user:password'
-	};
-
-	// Base url of the wp endpoint
-	var url = "http://gront.fr/wp-json/wp/v2/";
-
-
-	return {
-		simpleReq: function () {
-			return true;
-		}
-	}
-});
