6d7499148020f22df29aba18c266c2dce67d6a88
Matthias Beaupère <matthias.beaupere@gmail.com>
2016-03-19 14:33:33 +0100

    adaptation sur android des requetes de produit + erreur de connection

--- www/js/bdd.js
+++ www/js/bdd.js
@@ -5,18 +5,20 @@ var url = '/rest-api/wp-json/wp/v2/';
 var wc = new WooCommerceAPI({
   consumerKey: 'ck_aabd0e6985206e1ea8945e6d1f203bd6f88bd52c',
   consumerSecret: 'cs_c69ef1e13e5165daf06a717d5735665c230aa2fc',
+  //url: 'http://gront.fr'
   url: 'http://localhost:8100/rest-api/',
   proxy: 'http://gront.fr/'
 });
 
 var wp = new WP({
   endpoint: 'http://localhost:8100/rest-api/wp-json/',
+  //endpoint: 'http://gront.fr/wp-json/',
   username : 'appmobile',
   password : 'Jc29JQi^5jtq@@yT($0)4e#('
 });
 
 grontApp.factory('bdd', function($http, $q) {
-	// Using a factory to fetch requests from WordPress site using WP REST API
+	// Using a factory to fetch requests from WordPress site using WP and WC REST APIs
 
 	return {
     getUsers: function() {
@@ -32,7 +34,12 @@ grontApp.factory('bdd', function($http, $q) {
     getProducts: function() {
       var res = $q.defer();
       wc.get('products', function(err, data, result) {
-        res.resolve(JSON.parse(result).products);
+        if(err == null) {
+          res.resolve({data: JSON.parse(result).products, err: false});
+        }
+        else {
+          res.resolve({data: [], err: 'Impossible de se connecter.'});
+        }
       });
       return res.promise;
     }
--- www/js/controllers/store-ctrl.js
+++ www/js/controllers/store-ctrl.js
@@ -1,11 +1,35 @@
-grontApp.controller('StoreCtrl', function ($scope, bdd, $ionicLoading) {
-  //$scope.products = bdd.getProducts();
+grontApp.controller('StoreCtrl', function ($scope, bdd, $ionicLoading, $ionicPopup) {
+
+  var loadProducts = function () {
+    bdd.getProducts().then(function (res) {
+      $scope.products = res.data;
+      if(res.err) {
+        $ionicPopup.show(connectionErrorPopup(res.err));
+      }
+      $ionicLoading.hide();
+    });
+  };
+
+  var connectionErrorPopup = function (error) {
+    return {
+      title: 'Erreur de connexion',
+        template: error,
+      scope: $scope,
+      buttons: [
+        {text: 'Annuler'},
+        {
+          text: 'Réessayer',
+          type: 'button-positive',
+          onTap: loadProducts
+        }
+      ]
+    }
+  };
+
   $ionicLoading.show({
     template: 'Chargement de la boutique...'
   });
-  bdd.getProducts().then(function (res) {
-    $scope.products = res ? res:"No results";
-    $ionicLoading.hide();
-  });
+
+  loadProducts();
 });
 
