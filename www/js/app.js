1093775d0e0b26f7138053b5f389b3b85ac8dd9f
Matthias Beaupère <matthias.beaupere@gmail.com>
2016-03-21 15:03:44 +0100

    ajout d'un controleur séparé pour la page de connexion

--- www/js/app.js
+++ www/js/app.js
@@ -22,25 +22,6 @@ grontApp.controller('ConnexionCtrl', function ($scope) {
 
 });
 
-grontApp.controller('InscriptionCtrl', function ($scope, $rootScope, bdd) {
-
-  // Subscribing of a user
-  $scope.register = function () {
-
-    // Checking email length
-    // TODO check format
-    //if ($scope.email.length < 3) {
-    //  alert("L'adresse email est trop courte. Expected : > 3 Got : " + $scope.email.length);
-    //  return null;
-    //}
-    // TODO : check first and last name
-
-    bdd.userExists($scope.email).then(function(emailAlreadyRegistered) {
-
-    })
-  }
-});
-
 grontApp.config(function ($stateProvider, $urlRouterProvider) {
   $stateProvider.state('home', {
     url: '/home',
--- www/js/controllers/cyclo-ctrl.js
+++ www/js/controllers/cyclo-ctrl.js
@@ -1,4 +1,10 @@
 grontApp.controller('CycloCtrl', function ($scope, cart) {
   $scope.cart = cart.getProducts();
+
+  $scope.cyclos = [
+    {address: '25 rue du corbeau 15e arrondissement'},
+    {address: '18 rue des 3 mouettes 8e arrondissement'},
+    {address: '1 rue de l\'étoile 3e arrondissement'}
+  ];
 });
 
